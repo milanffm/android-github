1d02b27e0e0e79b05d955d32b6209e33d2aa5d74
Milan Peters <m.peters@graphicon.de>
2017-10-24 23:22:04 +0200

    init commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+
+import { OrganisationsPageModule } from '../pages/organisations/organisations.module';
+import { ReposPageModule } from '../pages/repos/repos.module';
+import { UsersPageModule } from '../pages/users/users.module';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+@NgModule({
+  declarations: [
+    MyApp
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    OrganisationsPageModule,
+    ReposPageModule,
+    UsersPageModule
+
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ src/pages/users/users.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-users',
+  templateUrl: 'users.html'
+})
+export class UsersPage {
+
+  constructor(public navCtrl: NavController) {}
+
+  ionViewDidLoad() {
+    console.log('Hello Users Page');
+  }
+}
